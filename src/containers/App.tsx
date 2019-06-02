4ab316bd0be98389d2c1b23b189c0ed481620566
unachoza <ariannachoza@Ariannas-MBP.fios-router.home>
2019-06-01 20:26:03 -0400

    adding typescript and boiler plate react

---
+++ src/containers/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Component {
+    render(){
+        return(
+            <div>Learning Hooks Y'all!!</div>
+        )
+    }
+}
+
+export default App
